7a909f6afd64791101e0c5c92dd1310b341f990f
鳥越幸樹 <niaoyuexingshu@gmail.com>
2023-09-30 00:46:14 +0900

    fix: ログアウト処理の移動

--- lib/features/auth/presentation/user_profile.dart
+++ lib/features/auth/presentation/user_profile.dart
@@ -1,20 +1,52 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/features/auth/application/logout_controller.dart';
 import 'package:todo_app/features/auth/data/auth_repository_impl.dart';
+import 'package:todo_app/router/router.gr.dart';
+import 'package:todo_app/utils/widget/loading.dart';
+import 'package:todo_app/utils/widget/sccaffold_messenger_service.dart';
 
 @RoutePage()
-class UserProfile extends ConsumerWidget {
-  const UserProfile({super.key});
+class UserProfilePage extends ConsumerWidget {
+  const UserProfilePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentUser = ref.watch(authRepositoryImplProvider).currentUser!;
+    /// ログアウト処理
+    ref.listen(logoutControllerProvider, (_, state) async {
+      if (state.isLoading) {
+        ref.watch(overlayLoadingProvider.notifier).update((state) => true);
+        return;
+      }
+      await state.when(data: (_) async {
+        ref.watch(overlayLoadingProvider.notifier).update((state) => false);
+        ref.read(scaffoldMessengerServiceProvider).showSnackBar('ログアウトしました');
+        context.router.replace(const LoginRoute());
+      }, error: (e, s) {
+        ref.watch(overlayLoadingProvider.notifier).update((state) => false);
+        ref.read(scaffoldMessengerServiceProvider).showSnackBar('エラーが発生しました。');
+      }, loading: () async {
+        ref.watch(overlayLoadingProvider.notifier).update((state) => true);
+      });
+    });
+
+    final state = ref.watch(logoutControllerProvider);
+
+    final currentUser = ref.watch(authRepositoryImplProvider).currentUser;
     return Center(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(currentUser.email!),
+        Text(currentUser == null ? '' : currentUser.email!),
+        ElevatedButton(
+          onPressed: state.isLoading
+              ? null
+              : () {
+                  ref.read(logoutControllerProvider.notifier).logout();
+                },
+          child: const Text('ログアウト'),
+        ),
       ],
     ));
   }
