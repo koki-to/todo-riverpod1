61f0ae085e7250a5ef0602454a41f554f1195b2c
鳥越幸樹 <niaoyuexingshu@gmail.com>
2023-09-29 11:42:40 +0900

    feat: BottomNavigationBarの実装

---
+++ lib/features/home/application/bottom_navigation_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum PageType {
+  Todo,
+  Profile,
+}
+
+final bottomNavigationNotifierProvider =
+    NotifierProvider<BottomNavigationNotifier, PageType>(
+        BottomNavigationNotifier.new);
+
+class BottomNavigationNotifier extends Notifier<PageType> {
+  @override
+  PageType build() {
+    return PageType.Todo;
+  }
+
+  void changePage(PageType pageType) {
+    state = pageType;
+  }
+}
---
+++ lib/features/home/presentation/widget/custom_bottom_navigation_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/features/home/application/bottom_navigation_controller.dart';
+
+class CustomBottomNavigationBar extends ConsumerWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentPage = ref.watch(bottomNavigationNotifierProvider);
+    return BottomNavigationBar(
+      backgroundColor: Colors.blueAccent,
+      selectedItemColor: Colors.white,
+      currentIndex: PageType.values.indexOf(currentPage),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.toc,
+            ),
+            label: 'todo'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'プロフィール'),
+      ],
+      onTap: (int index) {
+        final pageType = PageType.values[index];
+        ref
+            .read(bottomNavigationNotifierProvider.notifier)
+            .changePage(pageType);
+      },
+    );
+  }
+}
