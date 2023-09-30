c8748c5a3b6bcffa2ec19b38493d4fe3edcb1f56
鳥越幸樹 <niaoyuexingshu@gmail.com>
2023-09-30 13:16:18 +0900

    feat: shared_preferencesの初期化実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/app.dart';
 import 'package:todo_app/firebase_options.dart';
+import 'package:todo_app/utils/shared_preferences_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(ProviderScope(child: MyApp()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
